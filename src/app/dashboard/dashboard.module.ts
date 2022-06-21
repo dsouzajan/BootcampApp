830038f0ae95c5f8af1d0f38aedb2e1bd94bfbf5
Janice Dsouza <janice.dsouza@nielseniq.com>
2022-06-21 12:04:00 -0400

    First Commit - Changes

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VideoDashboardComponent } from './video-dashboard/video-dashboard.component';
+
+
+
+@NgModule({
+  declarations: [
+    VideoDashboardComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DashboardModule { }
